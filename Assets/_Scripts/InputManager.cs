7fd7a0b4002b68ed6cd23bbabad6319be059a260
HyungsukChoi <117467777+HyungsukChoi@users.noreply.github.com>
2023-09-07 13:33:15 +0900

    Initial commit

---
+++ Assets/_Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Camera fpsCam;
+    public Transform PlayerTr;
+    public float moveSpeed = 5f; // 이동 속도
+    public float rotSpeed = 3f;      // 회전 속도
+    public float currentRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Vertical");
+        float moveZ = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * moveX * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.right * moveZ * moveSpeed * Time.deltaTime);
+
+        float rotateX = Input.GetAxis("Mouse Y") * rotSpeed;
+        float rotateY = Input.GetAxis("Mouse X") * rotSpeed;
+
+        currentRot = rotateX;
+        // 특정각도를 넘어가지 않게 예외처리
+        currentRot = Mathf.Clamp(currentRot, -80f, 80f);
+
+        this.transform.localRotation *= Quaternion.Euler(0, rotateY, 0);
+        fpsCam.transform.localEulerAngles = new Vector3(currentRot, 0, 0);
+    }
+}
---
+++ Assets/_Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+    private float r = 0.0f;
+
+    float moveSpeed = 5.0f;
+    float rotSpeed = 180f;
+    Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+    void Move()
+    {
+        h= Input.GetAxis("Horizontal");
+        v= Input.GetAxis("Vertical");
+        //전후좌우 이동 방향 벡터 계산
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        //Translate(이동 방향 * 속도 * 변위값 * Time.deltaTime, 기준좌표)
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+        //Vector3.up 축을 기준으로 rotSpeed만큼의 속도로 회전
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
